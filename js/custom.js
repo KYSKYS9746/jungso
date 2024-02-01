a2b3ff238e318fff1ecf9e4e771f4efb5ffa21e8
KYSKYS9746 <148739304+KYSKYS9746@users.noreply.github.com>
2024-02-01 18:18:57 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function (){
+    var swiper = new Swiper(".main_visual_slide", {
+    
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+          },
+        
+      });
+
+      AOS.init();
+});
+
